10f336f3dae8cfe8ff931569bd380f3b09bc302b
Alessandro <admin@rthink.co>
2024-06-27 05:05:31 +0000

    update text

--- quickstart-automate/main.go
+++ quickstart-automate/main.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at    
+// You may obtain a copy of the License at
 //
 //     https://www.apache.org/licenses/LICENSE-2.0
 //
@@ -19,5 +19,5 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, universe2!")
+	fmt.Println("Hello, universe3!")
 }
